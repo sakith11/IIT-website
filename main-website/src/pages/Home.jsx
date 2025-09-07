a37060a2db3fd6e5240672b511981939871ce176
sakith11 <57568111+sakith11@users.noreply.github.com>
2025-09-07 23:00:57 +0530

    sakith11-add mobile view for home and map pages

--- main-website/src/pages/Home.jsx
+++ main-website/src/pages/Home.jsx
@@ -20,44 +20,33 @@ export default function Home() {
                         </Link>
                     </div>
                     <div className='right'>
-                        <table className="custom-table">
-                            <tbody>
-                                <tr>
-                                    <td colSpan="3" className="merged-cell top">
-                                        <div className="cell-content">
+                        <div className="custom-grid">
+                            <div className="grid-item full-width top">
+                                <div className="cell-content">
+                                    <h2>Common Guidelines</h2>
+                                    <img src="./images/image 36.png" className="cell-bg-image" />
+                                    <button className="cell-button">
+                                        <i className="fas fa-arrow-right"></i>
+                                    </button>
+                                </div>
+                            </div>
 
-                                            <h2>Common Guidlines</h2>
-                                            <img src="./images/image 36.png" className="cell-bg-image" />
+                            <div className="grid-item bottom">
+                                <div className="cell-content">
+                                    <h2>Meet the team</h2>
+                                    <img src="./images/image 37.png" className="cell-bg-image" />
+                                    <button className="cell-button"><i className="fas fa-arrow-right"></i></button>
+                                </div>
+                            </div>
 
-                                            <button className="cell-button">
-                                                <i className="fas fa-arrow-right"></i>
-                                            </button>
-
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td colSpan="3" className="spacer-row"></td>
-                                </tr>
-                                <tr>
-                                    <td className="merged-cell bottom">
-                                        <div className="cell-content">
-                                            <h2>Meet the team</h2>
-                                            <img src="./images/image 37.png" className="cell-bg-image" />
-                                            <button className="cell-button"><i className="fas fa-arrow-right"></i></button>
-                                        </div>
-                                    </td>
-                                    <td className="cell-gap">&nbsp;</td>
-                                    <td className="merged-cell bottom">
-                                        <div className="cell-content">
-                                            <h2>Roles </h2>
-                                            <img src="./images/image 38.png" className="cell-bg-image" />
-                                            <button className="cell-button"><i className="fas fa-arrow-right"></i></button>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                            <div className="grid-item bottom">
+                                <div className="cell-content">
+                                    <h2>Roles</h2>
+                                    <img src="./images/image 38.png" className="cell-bg-image" />
+                                    <button className="cell-button"><i className="fas fa-arrow-right"></i></button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
